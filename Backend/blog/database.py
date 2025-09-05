e79a86de612767d803c9c424b035d3b0e829b02a
Himanshukotia9 <himanshukotia9@gmail.com>
2025-09-05 20:58:04 +0530

    Connection string changes made

--- Backend/blog/database.py
+++ Backend/blog/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SUPABASE_DATABASE_URL = 'postgresql://postgres:IVT9YmdzrXE41ghA@db.lqhmjscbjdcaqgwjlieg.supabase.co:5432/postgres'
+SUPABASE_DATABASE_URL = 'postgresql://postgres:IVT9YmdzrXE41ghA@db.lqhmjscbjdcaqgwjlieg.supabase.co:5432/postgres?sslmode=require'
 
 engine = create_engine(SUPABASE_DATABASE_URL)
 
