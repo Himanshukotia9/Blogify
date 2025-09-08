93b1e96d4faaf2f18f7a9a5734b2cc55b2d7dcdf
Himanshukotia9 <himanshukotia9@gmail.com>
2025-09-08 20:31:15 +0530

    minor updates

--- Backend/blog/database.py
+++ Backend/blog/database.py
@@ -3,12 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SUPABASE_DATABASE_URL = 'postgresql://postgres:IVT9YmdzrXE41ghA@db.lqhmjscbjdcaqgwjlieg.supabase.co:5432/postgres?sslmode=require'
+SUPABASE_DATABASE_URL = 'postgresql://postgres:IVT9YmdzrXE41ghA@db.lqhmjscbjdcaqgwjlieg.supabase.co:5432/postgres'
 
-engine = create_engine(
-    SUPABASE_DATABASE_URL,
-    connect_args={"sslmode": "require"}
-    )
+engine = create_engine(SUPABASE_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind = engine, autocommit=False, autoflush=False)
 
